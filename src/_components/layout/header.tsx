a9a4b71c82379315ea45efde3d1859ad3b9c31fd
choitjddn0311 <cswcjw311427@naver.com>
2025-12-15 17:40:35 +0900

    feat: create header

---
+++ src/_components/layout/header.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import ThemeToggle from "../common/themeToggle";
+
+const Header = () => {
+    return (
+        <header className="w-full h-25 flex justify-center bg-background text-foreground">
+            <div className="w-300 flex justify-between items-center ">
+                <Link href="/" className="w-50 h-full content-center font-bold text-4xl text-cyan-400">
+                    <h1 className="group">S<sub className="text-s hidden group-hover:inline">ung</sub>W<sub className="text-s hidden group-hover:inline">oo</sub> dev</h1>
+                </Link>
+                <div className="w-250 flex justify-end gap-10">
+                    <nav className="w-200 h-full flex justify-end">
+                        <ul className="w-full h-full flex justify-end gap-10 items-center">
+                            <li className="font-semibold"><Link href="/">home</Link></li>
+                            <li className="font-semibold"><Link href="/about">about</Link></li>
+                            <li className="font-semibold"><Link href="/posts">posts</Link></li>
+                            <li className="font-semibold"><Link href="/contacts">contact</Link></li>
+                        </ul>
+                    </nav>
+                    <ThemeToggle/>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
