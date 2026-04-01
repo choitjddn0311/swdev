705f2bd5e15fbb89951e25b13feead8f37f1ac99
choitjddn0311 <cswcjw311427@naver.com>
2026-04-01 12:26:12 +0900

    feat: change structure as a components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,50 @@ import { FaGithub, FaInstagram, FaLongArrowAltDown,FaHtml5,FaCss3Alt,FaJs,FaReac
 import { SiVelog,SiOracle,SiMysql } from "react-icons/si";
 import { MdOutlineEmail } from "react-icons/md";
 import Link from "next/link";
-import { PiHandWaving } from "react-icons/pi";
 import Image from "next/image";
+import StackCard from "@/components/common/stackCard";
 
 const Home = () => {
+  // stack을 정의할때 구조는 다음과 같다: 스텍명(language name) , icon() from react-icons, iconColor()
+  const FrontStack = [
+    {stack: "html", icon: <FaHtml5/> , iconColor: "text-orange-500"},
+    {stack: "css" , icon: <FaCss3Alt/> , iconColor: "text-sky-500"},
+    {stack: "js" , icon: <FaJs/> , iconColor: "text-amber-400"},
+    {stack: "react" , icon: <FaReact/> , iconColor: "text-blue-400"}
+  ]
+  const BackStack = [
+    {stack: "php", icon: <FaPhp/> , iconColor: "text-foreground"},
+    {stack: "nodejs" , icon: <FaNodeJs/> , iconColor: "text-green-600"},
+    {stack: "jsp" , icon: <FaJava/> , iconColor: "text-red-500"}
+  ]
+  const DbStack = [
+    {stack: "oracle" , icon: <SiOracle/> , iconColor: "text-red-600"},
+    {stack: "jsp" , icon: <SiMysql/> , iconColor: "text-cyan-700"}
+  ]
+  const ServerStack = [
+    {stack: "linux", icon: <FaLinux/> , iconColor: "text-foreground"},
+    {stack: "ubuntu" , icon: <FaUbuntu/> , iconColor: "text-orange-500"},
+    {stack: "docker" , icon: <FaDocker/> , iconColor: "text-blue-600"}
+  ]
+  // 입상 기록을 정의할때 구조는 다음과 같다: 대회진행 년도(contest Year), 대회명(contestName), 수상명(award)
+  const AwardRecord = [
+    {year: "2024", contestName: "인천지방기능경기대회 웹 디자인 및 개발" , award: "동메달"},
+    {year: "2025", contestName: "인천지방기능경기대회 웹 디자인 및 개발" , award: "은메달"},
+    {year: "2025", contestName: "광주전국기능경기대회 웹 디자인 및 개발" , award: "장려상(14위)"},
+    {year: "2025", contestName: "progate hackathon" , award: "2등"}
+  ]
+  // 자격증을 정의할때 구조는 다음과 같다: 취득 상세일자(license get detail Date), 자격증명(license name) 만약 자격증의 이름이 바뀌었다면 현재 바뀐 이름이 먼저 나오고 뒤에 (구 ~~)라고 붙인다.
+  const license = [
+    {getDate: "2025.01.06" , licenseName: "프로그래밍기능사(구 정보처리기능사)"},
+    {getDate: "2025.06.05" , licenseName: "웹디자인개발기능사"},
+    {getDate: "2025.03.19" , licenseName: "정보처리산업기사(과정평가형)"}
+  ]
   return (
     <div>
         <main className="w-full flex items-center flex-col bg-background text-foreground">
-          <div className="w-300 flex flex-col justify-center items-center h-210 gap-30">
-              <div>
-                <h1 className="text-4xl text-center flex justify-center">안녕하세요. <span className="bg-cyan-300">무궁무진한 개발자</span>를 꿈꾸는 <span className="text-cyan-500">최성우</span>입니다!<PiHandWaving/> </h1>
+          <div className="w-300 flex flex-col justify-center items-center h-210 gap-50">
+              <div className="flex flex-col gap-2">
+                <h1 className="text-4xl text-center">안녕하세요. <span className="bg-cyan-300">무궁무진한 개발자</span>를 꿈꾸는 <span className="text-cyan-500">최성우</span>입니다! </h1>
                 <h2 className="text-2xl text-center">hello. thank you for comming! i'm <span className="text-cyan-500">sungwoo Choi</span> && <span className="bg-cyan-300">dreaming fullstack developer</span></h2>
               </div>
               <div className="border-2 border-cyan-400 w-15 h-15 flex justify-center items-center rounded-[50%] animate-bounce text-xl hover:cursor-pointer">
@@ -22,69 +56,12 @@ const Home = () => {
           <div className="w-300 h-200 flex flex-col justify-start gap-20">
               <h1 className="text-4xl">아래는 제가 다룰 수 있는 <span className="bg-cyan-300">기술들</span>이에요.</h1>
               <div className="flex w-full flex-wrap justify-between items-center h-130">
-                  <ul className="w-130 h-50 p-3 border-2 border-maincolor rounded-lg flex flex-col justify-start gap-3 bg-background">
-                    <h1 className="text-xl ">frontend</h1>
-                    <div className="flex justify-between items-center">
-                      <li className="w-30 text-center text-foreground">html</li>
-                      <li className="w-30 text-center text-foreground">css</li>
-                      <li className="w-30 text-center text-foreground">js</li>
-                      <li className="w-30 text-center text-foreground">react</li>
-                    </div>
-                    <div className="flex justify-between items-center">
-                      <li className="w-30 h-25 flex justify-center items-center text-2xl text-orange-500"><FaHtml5/></li>
-                      <li className="w-30 h-25 flex justify-center items-center text-2xl text-sky-500"><FaCss3Alt/></li>
-                      <li className="w-30 h-25 flex justify-center items-center text-2xl text-amber-400"><FaJs/></li>
-                      <li className="w-30 h-25 flex justify-center items-center text-2xl text-blue-400"><FaReact/></li>
-                    </div>
-                  </ul>
-
-                  <ul className="w-130 h-50 p-3 border-2 border-maincolor rounded-lg flex flex-col justify-start gap-3">
-                    <h1 className="text-xl ">backend</h1>
-                    <div className="flex justify-between items-center">
-                      <li className="w-30 text-center text-foreground">php</li>
-                      <li className="w-30 text-center text-foreground">node.js</li>
-                      <li className="w-30 text-center text-foreground">jsp</li>
-                      <li className="w-30 text-center text-foreground"></li>
-                    </div>
-                    <div className="flex justify-between items-center">
-                      <li className="w-30 h-25 flex justify-center items-center text-2xl text-foreground"><FaPhp/></li>
-                      <li className="w-30 h-25 flex justify-center items-center text-2xl text-green-600"><FaNodeJs/></li>
-                      <li className="w-30 h-25 flex justify-center items-center text-2xl text-red-500"><FaJava/></li>
-                      <li className="w-30 h-25 flex justify-center items-center"></li>
-                    </div>
-                  </ul>
-
-                  <ul className="w-130 h-50 p-3 border-2 border-maincolor rounded-lg flex flex-col justify-start gap-3">
-                    <h1 className="text-xl ">database</h1>
-                    <div className="flex justify-between items-center">
-                      <li className="w-30 text-center text-foreground">oracle</li>
-                      <li className="w-30 text-center text-foreground">mysql</li>
-                      <li className="w-30 text-center text-foreground"></li>
-                      <li className="w-30 text-center text-foreground"></li>
-                    </div>
-                    <div className="flex justify-between items-center">
-                      <li className="w-30 h-25 flex justify-center items-center text-2xl text-red-600"><SiOracle/></li>
-                      <li className="w-30 h-25 flex justify-center items-center text-2xl text-cyan-700"><SiMysql/></li>
-                      <li className="w-30 h-25 flex justify-center items-center"></li>
-                      <li className="w-30 h-25 flex justify-center items-center"></li>
-                    </div>
-                  </ul>
-
-                  <ul className="w-130 h-50 p-3 border-2 border-maincolor rounded-lg flex flex-col justify-start gap-3">
-                    <h1 className="text-xl ">server</h1>
-                    <div className="flex justify-between items-center">
-                      <li className="w-30 text-center text-foreground">linux</li>
-                      <li className="w-30 text-center text-foreground">ubuntu</li>
-                      <li className="w-30 text-center text-foreground">docker</li>
-                      <li className="w-30 text-center text-foreground"></li>
-                    </div>
-                    <div className="flex justify-between items-center">
-                      <li className="w-30 h-25 flex justify-center items-center text-2xl text-foreground"><FaLinux/></li>
-                      <li className="w-30 h-25 flex justify-center items-center text-2xl text-orange-500"><FaUbuntu/></li>
-                      <li className="w-30 h-25 flex justify-center items-center text-2xl text-blue-600"><FaDocker/></li>
-                      <li className="w-30 h-25 flex justify-center items-center"></li>
-                    </div>
-                  </ul>
+                {/* 구조를 수정하려면 components -> common -> stackCard.tsx */}
+                {/* 스텍을 추가하려면 상단에 ~~Stack 변수에 추가하면 된다 */}
+                <StackCard title="frontend" items={FrontStack}/>
+                <StackCard title="backend" items={BackStack}/>
+                <StackCard title="database" items={DbStack}/>
+                <StackCard title="server" items={ServerStack}/>
               </div>
           </div>
           <div className="w-300 h-160 flex">
@@ -120,17 +97,16 @@ const Home = () => {
                   <li><span className="font-bold">경력:</span> 산학일체형도제학교 8기 <span className="bg-maincolor"><Link href="https://www.slexn.com/" className="underline decoration-1">(유)슬렉슨</Link></span> 근무 | 2024.08.01 ~ 2026.01.30</li>
                   <li><span className="font-bold">수상실적:</span> 
                     <ul>
-                      <li>2024 인천지방기능경기대회 웹 디자인 및 개발 <span className="bg-maincolor">동메달</span></li>
-                      <li>2025 인천지방기능경기대회 웹 디자인 및 개발 <span className="bg-maincolor">은메달</span></li>
-                      <li>2025 광주전국기능경기대회 웹 디자인 및 개발 <span className="bg-maincolor">장려상(14위)</span></li>
-                      <li>2025 progate hackathon <span className="bg-maincolor">2등</span></li>
+                      {AwardRecord.map((items,index) => (
+                        <li key={index}>{items.year} {items.contestName} <span className="bg-maincolor">{items.award}</span></li>
+                      ))}
                     </ul>
                   </li>
                   <li><span className="font-bold">자격증:</span> 
                     <ul>
-                      <li>2025.01.06 정보처리기능사(프로그래밍기능사)</li>
-                      <li>2025.06.05 웹디자인개발기능사</li>
-                      <li>2025.03.19 정보처리산업기사(과정평가형)</li>
+                      {license.map((items,index) => (
+                        <li key={index}>{items.getDate} {items.licenseName}</li>
+                      ))}
                     </ul>
                   </li>
                 </ul>
